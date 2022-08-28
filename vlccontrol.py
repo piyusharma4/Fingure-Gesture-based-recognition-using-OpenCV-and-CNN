bb135de04060698a29070ab39989982130c7cc62
piyusharma4 <34770695+piyusharma4@users.noreply.github.com>
2022-08-28 19:30:08 +0530

    Add files via upload

---
+++ vlccontrol.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 11 21:02:12 2020
+
+@author: justp
+"""
+
+import vlc
+
+player=vlc.MediaPlayer(r'B:\The.Many.Worlds.of.Quantum.Mechanics.1080p.HDTV.x264.AAC.MVGroup.org.mp4')
+player.play()
+
+player.
